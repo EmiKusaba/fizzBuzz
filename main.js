4ca3597ead32c705d77762ce77f3a864c1f91556
EmiKusaba <eeemi615@gmail.com>
2021-11-10 21:47:43 -0600

    fixed everything

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-let clicks = 0
+let number = 0
 
 const myFunc = (i) => {
   if (document.getElementById("minus"))
-    for (let i = 0; i <= 100; i--) {
+    for (let i = 100; i >= 0; i--) {
       if (i % 15 == 0) console.log("FizzBuzz");
       else if (i % 3 == 0) console.log("Fizz");
       else if (i % 5 == 0) console.log("Buzz");
       else console.log(i);
-      i--
     }
   if (document.getElementById("plus"))
     for (let i = 0; i <= 100; i++) {
@@ -17,7 +16,7 @@ const myFunc = (i) => {
       else console.log(i);
       i++
     }
-    showNum()
+  showNum()
 }
 
 const showNum = () => {
@@ -25,7 +24,31 @@ const showNum = () => {
   console.log(newNum)
   let num = document.getElementById("num")
   const p = document.createElement("p")
-p.innerHTML(newNum)
+  p.innerHTML(newNum)
+}
+
+const plus = () => {
+  number += 1
+
+  let value = number
+
+  if (number % 15 == 0) value = "FizzBuzz"
+  else if (number % 3 == 0) value = "Fizz"
+  else if (number % 5 == 0) value = "Buzz"
+
+  document.getElementById("num").innerHTML = value
+}
+
+const minus = () => {
+  number -= 1
+
+  let value = number
+  
+  if (number % 15 == 0) value = "FizzBuzz"
+  else if (number % 3 == 0) value = "Fizz"
+  else if (number % 5 == 0) value = "Buzz"
+
+  document.getElementById("num").innerHTML = value
 }
 
 
